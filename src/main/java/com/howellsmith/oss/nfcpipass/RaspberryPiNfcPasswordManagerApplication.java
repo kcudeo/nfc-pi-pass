d0c095f0ee2250ab34e2181dd4a4a96127500076
Taylor Howellsmith <taylor@howellsmith.com>
2021-01-09 16:48:48 -0500

    Sign

---
+++ src/main/java/com/howellsmith/oss/nfcpipass/RaspberryPiNfcPasswordManagerApplication.java
@@ -0,0 +1,13 @@
+package com.howellsmith.oss.nfcpipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryPiNfcPasswordManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaspberryPiNfcPasswordManagerApplication.class, args);
+    }
+
+}
