cfb3339c420daed3fae10e1719bdfa2daa40d912
Taylor Howellsmith <taylor@howellsmith.com>
2021-01-23 17:38:47 -0500

    Add Unified Logging Service
    The unified logging service allows for cleaner JSON based logging.

---
+++ src/main/java/com/howellsmith/oss/nfcpipass/config/StringConstants.java
@@ -0,0 +1,11 @@
+package com.howellsmith.oss.nfcpipass.config;
+
+/**
+ * A class used to hold string constants.
+ */
+public class StringConstants {
+
+    public static final String ILLEGAL_ARGUMENT_NULL_MARKER = "ILLEGAL_ARGUMENT_NULL_MARKER";
+    public static final String ILLEGAL_ARGUMENT_NULL_MESSAGE = "The supplied argument cannot be null.";
+
+}
